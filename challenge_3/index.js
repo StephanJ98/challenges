d8ea23a58b77fdbd00c43cb2e43d1d101eafbd8d
StephanJ98 <diego.jeandon@gmail.com>
2023-07-18 14:28:43 +0200

    :zap: challenge_3 solution

--- challenge_3/index.js
+++ challenge_3/index.js
@@ -2,7 +2,14 @@ const salt = new Date().getTime() % Math.round(Math.random() * 50)
 const phrase = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper mi nisl, et sollicitudin elit bibendum sed. Nulla ut ante semper, consequat quam ac, efficitur elit. Curabitur gravida lacus nec dolor ultrices ultricies. Nunc rutrum mauris eget nisi congue, ac dapibus lectus laoreet. Nullam volutpat purus vitae ligula aliquet, in mattis lectus lacinia. Mauris auctor mauris a dapibus luctus. In at tincidunt arcu. Morbi non faucibus mi, eget volutpat felis. Proin vitae luctus mi, in suscipit metus.`
 
 const encryptPhrase = (phrase) => {
-    // ToDo
+    let encryptedPhrase = ''
+
+    for (let i = 0; i < phrase.length; i++) {
+        const charCode = phrase.charCodeAt(i)
+        const encryptedCharCode = charCode + salt
+        encryptedPhrase += String.fromCharCode(encryptedCharCode)
+    }
+    return encryptedPhrase
 }
 
 const decryptPhrase = (encryptedPhrase) => {
